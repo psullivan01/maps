b33ff2c83b8201e12deb9d4972b3e4f3663e7a54
psullivan01 <psullivan@hearstautos.com>
2018-07-09 12:13:05 -0400

    remove PredictiveSearch component relic

--- maps/src/components/PredictiveSearch.js
+++
@@ -1,6 +0,0 @@
-const autocompleteFormField = document.getElementById('street-address-field');
-
-const autocomplete = new google.maps.places.Autocomplete((autocompleteFormField), {
-    types: ['address'],
-    componentRestrictions: ['us'],
-});
