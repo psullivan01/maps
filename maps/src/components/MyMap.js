93464f70cd123447b70a763ebf2431b838d1e795
psullivan01 <psullivan@hearstautos.com>
2018-07-09 11:29:11 -0400

    various asthetic changes

--- maps/src/components/MyMap.js
+++ maps/src/components/MyMap.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import L from 'leaflet';
 import curve from 'leaflet-curve';
+import '../styles/JourneyMap.css';
+import { RSA_PKCS1_OAEP_PADDING } from 'constants';
+import 'leaflet-rotatedmarker';
 
 const stamenTonerTiles = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png';
 const stamenTonerAttr = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>'
@@ -8,6 +11,35 @@ const mapCenter = [32.7759428, -96.7967532];
 const zoomLevel = 4;
 
 class MyMap extends Component {
+
+    toRad(coord) {
+        return coord * Math.PI / 180;
+    }
+
+    toDeg(rads) {
+        return rads * 180 / Math.PI;
+    }
+
+    getBearing(lat1, lng1, lat2, lng2) {
+        let initial = 210;
+
+        let dLat = this.toRad(lat2 - lat1),
+            dLng = this.toRad(lng2 - lng1);
+
+        lat1 = this.toRad(lat1);
+        lat2 = this.toRad(lat2);
+
+        let y = Math.sin(dLng) * Math.cos(lat2),
+            x = (Math.cos(lat1) * Math.sin(lat2)) - (Math.sin(lat1) * Math.cos(lat2) * Math.cos(dLng));
+
+        let bearing = this.toDeg(Math.atan2(y, x));
+
+        if (bearing < 0) {
+            bearing = 360 - Math.abs(bearing);
+        }
+
+        return initial - bearing;
+    }
 
     createCurves() {
         let initialCoords = []
@@ -24,27 +56,28 @@ class MyMap extends Component {
             let latlng1 = [initialCoords[i][0], initialCoords[i][1]],
                 latlng2 = [initialCoords[i+1][0], initialCoords[i+1][1]];
             
-            var offsetX = latlng2[1] - latlng1[1],
+            let offsetX = latlng2[1] - latlng1[1],
                 offsetY = latlng2[0] - latlng1[0];
             
-            var r = Math.sqrt( Math.pow(offsetX, 2) + Math.pow(offsetY, 2) ),
+            let r = Math.sqrt(Math.pow(offsetX, 2) + Math.pow(offsetY, 2) ),
                 theta = Math.atan2(offsetY, offsetX);
             
-            var thetaOffset = (3.14/10);
+            let thetaOffset = (3.14/10);
             
-            var r2 = (r/2)/(Math.cos(thetaOffset)),
+            let r2 = (r/2)/(Math.cos(thetaOffset)),
                 theta2 = theta + thetaOffset;
             
-            var midpointX = (r2 * Math.cos(theta2)) + latlng1[1],
+            let midpointX = (r2 * Math.cos(theta2)) + latlng1[1],
                 midpointY = (r2 * Math.sin(theta2)) + latlng1[0];
             
-            var midpointLatLng = [midpointY, midpointX];
+            let midpointLatLng = [midpointY, midpointX];
             
             latlngs.push(latlng1, midpointLatLng, latlng2);
 
             let pathOptions = {
-                color: 'blue',
-                weight: 2
+                color: '#1e90ff',
+                weight: 2,
+                dashArray: '7, 5'
             }
 
             this.curvedPath = L.curve(
@@ -54,7 +87,9 @@ class MyMap extends Component {
                         latlng2
                 ], pathOptions).addTo(this.map);
 
-            console.log(this.curvedPath.getPath())
+            let bearing = this.getBearing(latlng1[0], latlng1[1], latlng2[0], latlng2[1]);
+
+            console.log(this.curvedPath.getPath(), '*bearing*', bearing);
 
             // find bezier extrema
             let t = .5,
@@ -71,10 +106,10 @@ class MyMap extends Component {
     }
 
     componentDidMount() {
-
         this.map = L.map('map', {
             center: mapCenter,
             zoom: zoomLevel,
+            attributionControl: false,
             layers: [
                 L.tileLayer(stamenTonerTiles, {
                     attribution: stamenTonerAttr
@@ -88,7 +123,15 @@ class MyMap extends Component {
     }
 
     render() {
-        return <div id="map"></div>
+        return ( 
+            <div id="outer">
+                <div id="inner">
+                    <div id="map">
+                        <div id="legend">My Map</div>
+                    </div>
+                </div>
+            </div>
+        )
     }
 }
 
